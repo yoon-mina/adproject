cd72a5e52ebb63d5d9bf5d5ef50dec05368fd193
Yoon <skwhjj@gmail.com>
2024-06-01 18:22:44 +0900

    Initial commit

---
+++ src/main/java/com/ms/adproject/AdprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ms.adproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ms/adproject/AdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.adproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
