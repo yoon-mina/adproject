a70b507b0183ada132fae876cfcea1da18fbb645
Yoon <skwhjj@gmail.com>
2024-06-01 18:54:57 +0900

    setting

--- src/main/java/com/ms/adproject/AdprojectApplication.java
+++ src/main/java/com/ms/adproject/AdprojectApplication.java
@@ -2,7 +2,9 @@ package com.ms.adproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AdprojectApplication {
 
@@ -10,4 +12,7 @@ public class AdprojectApplication {
         SpringApplication.run(AdprojectApplication.class, args);
     }
 
+    public String hello() {
+        return "hello";
+    }
 }
