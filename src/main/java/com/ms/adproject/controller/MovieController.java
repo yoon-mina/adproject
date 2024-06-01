31f9a76026a3ff9154866daef282b47d14a546a2
Yoon <skwhjj@gmail.com>
2024-06-02 01:27:55 +0900

    Movie select

--- src/main/java/com/ms/adproject/controller/MovieController.java
+++ src/main/java/com/ms/adproject/controller/MovieController.java
@@ -4,6 +4,10 @@ import com.ms.adproject.repository.MovieRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.Timestamp;
 import java.util.List;
 
 @Controller
@@ -20,5 +24,18 @@ public class MovieController {
         List<Movie> movies = movieRepository.findAll();
         model.addAttribute("movies", movies);
         return "movies/movies";
+    }
+
+    @GetMapping("/movies/new")
+    public String showMovieForm(Model model) {
+        model.addAttribute("movie", new Movie());
+        return "movies/new_movie";
+    }
+
+    @PostMapping("/movies/new")
+    public String addNewMovie(@ModelAttribute Movie movie) {
+        movie.setCreatedAt(new Timestamp(System.currentTimeMillis())); // 현재 시간을 설정
+        movieRepository.save(movie); // 영화 저장
+        return "redirect:/movies"; // 영화 목록 페이지로 리다이렉트
     }
 }
