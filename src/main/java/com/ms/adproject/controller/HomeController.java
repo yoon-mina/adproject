056b3006c4e51e7044d8d5967cd85dfbd8b46939
Yoon <skwhjj@gmail.com>
2024-06-07 03:14:00 +0900

    movie information direct (naver_search)

--- src/main/java/com/ms/adproject/controller/HomeController.java
+++ src/main/java/com/ms/adproject/controller/HomeController.java
@@ -5,7 +5,6 @@ import com.ms.adproject.repository.MovieRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
 
 @Controller
